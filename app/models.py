bd412921070e8e1859891695ce5c097bfe9e3ee0
Ursul Vladyslav <gora050@gmail.com>
2016-12-03 17:36:42 +0200

    Working with Models

--- app/models.py
+++ app/models.py
@@ -1,10 +1,24 @@
 from app import db
 from flask_restful import Api, Resource
 import sqlalchemy.types as types
+import json
 
 ROLE_USER = 0
 ROLE_VOLONTEER = 1
 ROLE_ADMIN = 2
+
+def to_json(model):
+    """ Returns a JSON representation of an SQLAlchemy-backed object.
+    """
+    json1 = {}
+    json1['fields'] = {}
+    json1['pk'] = getattr(model, 'id')
+
+    for col in model._sa_class_manager.mapper.mapped_table.columns:
+        json1['fields'][col.name] = getattr(model, col.name)
+
+    return json.dumps([json1])
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -14,7 +28,7 @@ class User(db.Model):
     password = db.Column(db.String(100))
     birth_date = db.Column(db.DateTime,nullable = True)
     region = db.Column(db.Integer,db.ForeignKey('region.id'),nullable = True)
-    status = db.Column(db.Integer,db.ForeignKey('volounteer_status'),nullable = True)
+    status = db.Column(db.Integer,db.ForeignKey('volounteer_status.id'),nullable = True)
     address = db.Column(db.String(200),nullable = True)
     email = db.Column(db.String(120), unique = True)
     aditional_info = db.Column(db.Text,nullable = True)
@@ -25,10 +39,12 @@ class User(db.Model):
         return '<User %r %r>' % (self.first_name,self.second_name)
 
 class Dog_Status(db.Model):
+    __tablename__ = 'dog_status'
     id = db.Column(db.Integer,primary_key = True)
     status_title = db.Column(db.String(300))
 
 class Volounteer_Status(db.Model):
+    __tablename__ = 'volounteer_status'
     id = db.Column(db.Integer,primary_key = True)
     status_title = db.Column(db.String(300))
 
@@ -39,6 +55,16 @@ class Region(db.Model):
 class Gender(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     gender_title = db.Column(db.String(300))
+
+    def __init__(self,  gender_title):
+        self.gender_title = gender_title
+
+    def add(self, resource):
+        db.session.add(resource)
+        return db.session.commit()
+
+    def to_dict(self):
+        return {"id":self.id, "gender_title":self.gender_title}
 
 class Size(db.Model):
     id = db.Column(db.Integer,primary_key = True)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,38 @@
 from app import app, api
-from flask_restful import Resource, Api
+from flask import request, jsonify, make_response
+from flask_restful import Resource, Api, reqparse
+import json
+from app import models
+
+
+parser = reqparse.RequestParser()
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+class Dogs(Resource):
+    def get(self):
+        users_query = models.Dog.query.all()
+        return users_query
+
+    def put(self):
+        return {"todo_id": 1}
+
+class Genders(Resource):
+    def get(self):
+        users_query = models.Gender.query.all()
+        res = {}
+        for i in range(len(users_query)):
+            res[i] = users_query[i].to_dict()
+        return res
+
+    def post(self):
+        try:
+            parser.add_argument('gender_title')
+            args = parser.parse_args()
+            gender = models.Gender(args['gender_title'])
+            gender.add(gender)
+            return {"status": 1}
+        except Exception as e:
+            return e
