605fd6e6ef200a62ed1b2fc6c9b2ecbf15e64918
yarsanich <nezn27@gmail.com>
2016-12-03 15:20:45 +0200

    create models v1

--- app/models.py
+++ app/models.py
@@ -1,24 +1,52 @@
 from app import db
-
+from flask_restful import Api, Resource
+import sqlalchemy.types as types
 
 ROLE_USER = 0
-ROLE_ADMIN = 1
+ROLE_VOLONTEER = 1
+ROLE_ADMIN = 2
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), unique = True)
+    first_name = db.Column(db.String(100))
+    second_name = db.Column(db.String(100))
+    phone_number = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    birth_date = db.Column(db.DateTime)
+    region = db.Column(db.String(200))
+    address = db.Column(db.String(200))
     email = db.Column(db.String(120), unique = True)
+    aditional_info = db.Column(db.Text)
+    credit_number = db.Column(db.String(200))
     role = db.Column(db.SmallInteger, default = ROLE_USER)
-    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return '<User %r %r>' % (self.first_name,self.second_name)
 
-class Post(db.Model):
+class Status(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    status_title = db.Column(db.String(300))
+
+class Region(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    region_title = db.Column(db.String(300))
+
+class Gender(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    gender_title = db.Column(db.String(300))
+
+class Size(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    size_title = db.Column(db.String(300))
+
+class Dog(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    age = db.Column(db.String(100))
+    aditional_info = db.Column(db.Text)
+    volnteer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    status = db.Column(db.Integer,db.ForeignKey('status.id'))
+    gender = db.Column(db.Integer,db.ForeignKey('gender.id'))
+    region = db.Column(db.Integer,db.ForeignKey('region.id'))
+    size = db.Column(db.Integer,db.ForeignKey('size.id'))
     def __repr__(self):
-        return '<Post %r>' % (self.body)
+        return '<Dog %d>' % (self.id)
